7112212f70c53c7c1e332edd5487a5a5d29a865f
LesnyRumcajs <LesnyRumcajs@users.noreply.github.com>
2020-12-22 22:40:32 +0100

    Optimized the data structures

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,26 +1,30 @@
-use std::io::{self, BufRead};
+use std::{
+    cmp::Ordering,
+    collections::{HashSet, VecDeque},
+    io::{self, BufRead},
+};
 
 fn main() {
     let (deck1, deck2) = read_decks(io::stdin().lock());
     println!("Day 22, part 1: {}", part1(deck1.clone(), deck2.clone()));
-    println!("Day 22, part 2: {}", part2(deck1.clone(), deck2.clone()));
+    println!("Day 22, part 2: {}", part2(deck1, deck2));
 }
 
-fn part1(mut deck1: Vec<u32>, mut deck2: Vec<u32>) -> u32 {
+fn part1(mut deck1: VecDeque<u32>, mut deck2: VecDeque<u32>) -> u32 {
     while !deck1.is_empty() && !deck2.is_empty() {
-        let card1 = *deck1.first().unwrap();
-        let card2 = *deck2.first().unwrap();
+        let card1 = deck1.pop_front().unwrap();
+        let card2 = deck2.pop_front().unwrap();
 
-        deck1.remove(0);
-        deck2.remove(0);
-        if card1 > card2 {
-            deck1.push(card1);
-            deck1.push(card2);
-        } else if card2 > card1 {
-            deck2.push(card2);
-            deck2.push(card1);
-        } else {
-            panic!("a draw!");
+        match card1.cmp(&card2) {
+            Ordering::Greater => {
+                deck1.push_back(card1);
+                deck1.push_back(card2);
+            }
+            Ordering::Less => {
+                deck2.push_back(card2);
+                deck2.push_back(card1);
+            }
+            Ordering::Equal => panic!("a draw!"),
         }
     }
     if deck1.is_empty() { deck2 } else { deck1 }
@@ -30,21 +34,16 @@ fn part1(mut deck1: Vec<u32>, mut deck2: Vec<u32>) -> u32 {
         .fold(0, |sum, (i, v)| sum + (i as u32 + 1) * v)
 }
 
-fn play_game(mut deck1: Vec<u32>, mut deck2: Vec<u32>) -> (u32, Vec<u32>) {
-    let mut cache: Vec<(Vec<u32>, Vec<u32>)> = Vec::new();
+fn play_game(mut deck1: VecDeque<u32>, mut deck2: VecDeque<u32>) -> (u32, VecDeque<u32>) {
+    let mut cache: HashSet<(VecDeque<u32>, VecDeque<u32>)> = HashSet::new();
     while !deck1.is_empty() && !deck2.is_empty() {
         let current = (deck1.clone(), deck2.clone());
-        if cache.contains(&current) {
-            return (1, deck1.clone());
+        if !cache.insert(current) {
+            return (1, deck1);
         }
 
-        cache.push(current);
-
-        let card1 = *deck1.first().unwrap();
-        let card2 = *deck2.first().unwrap();
-
-        deck1.remove(0);
-        deck2.remove(0);
+        let card1 = deck1.pop_front().unwrap();
+        let card2 = deck2.pop_front().unwrap();
 
         let result = if card1 <= deck1.len() as u32 && card2 <= deck2.len() as u32 {
             play_game(
@@ -59,25 +58,25 @@ fn play_game(mut deck1: Vec<u32>, mut deck2: Vec<u32>) -> (u32, Vec<u32>) {
         };
 
         if result == 1 {
-            deck1.push(card1);
-            deck1.push(card2);
+            deck1.push_back(card1);
+            deck1.push_back(card2);
         } else {
-            deck2.push(card2);
-            deck2.push(card1);
+            deck2.push_back(card2);
+            deck2.push_back(card1);
         }
     }
 
     if deck1.is_empty() {
-        return (2, deck2);
+        (2, deck2)
     } else if deck2.is_empty() {
-        return (1, deck1);
+        (1, deck1)
     } else {
         panic!("fiasco");
     }
 }
 
-fn part2(deck1: Vec<u32>, deck2: Vec<u32>) -> u32 {
-    play_game(deck1.clone(), deck2.clone())
+fn part2(deck1: VecDeque<u32>, deck2: VecDeque<u32>) -> u32 {
+    play_game(deck1, deck2)
         .1
         .iter()
         .rev()
@@ -85,9 +84,9 @@ fn part2(deck1: Vec<u32>, deck2: Vec<u32>) -> u32 {
         .fold(0, |sum, (i, v)| sum + (i as u32 + 1) * v)
 }
 
-fn read_decks<R: BufRead>(reader: R) -> (Vec<u32>, Vec<u32>) {
-    let mut deck1 = Vec::new();
-    let mut deck2 = Vec::new();
+fn read_decks<R: BufRead>(reader: R) -> (VecDeque<u32>, VecDeque<u32>) {
+    let mut deck1 = VecDeque::new();
+    let mut deck2 = VecDeque::new();
 
     let mut section = 0;
     for line in reader.lines().filter_map(Result::ok) {
@@ -96,14 +95,14 @@ fn read_decks<R: BufRead>(reader: R) -> (Vec<u32>, Vec<u32>) {
             continue;
         }
 
-        if line.starts_with("P") {
+        if line.starts_with('P') {
             continue;
         }
 
         if section == 0 {
-            deck1.push(line.parse().unwrap());
+            deck1.push_back(line.parse().unwrap());
         } else {
-            deck2.push(line.parse().unwrap());
+            deck2.push_back(line.parse().unwrap());
         }
     }
 
@@ -119,6 +118,6 @@ mod tests {
     fn test_solution() {
         let (deck1, deck2) = read_decks(BufReader::new(File::open("inputs/day22/1.txt").unwrap()));
         assert_eq!(part1(deck1.clone(), deck2.clone()), 30138);
-        assert_eq!(part2(deck1.clone(), deck2.clone()), 31587);
+        assert_eq!(part2(deck1, deck2), 31587);
     }
 }
